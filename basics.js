0053b3a2dc31e5e62303295dc8e909492981c03e
Jan Phillip Carreon <janphillipcarreon@Jans-MacBook-Pro.local>
2016-09-06 17:15:32 -1000

    created greet function

--- basics.js
+++ basics.js
@@ -11,11 +11,24 @@ person.age = 26;
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+ var canDrive;
+
+if (person.age > 16) {
+ 	canDrive = true;
+}else{
+	canDrive = false;
+}
+
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+ function greet(name){
+  console.log('Hello, my name is ' + name);
+ }
+ greet(person.name);
+
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
